d0d01d75826d46c19bb037f478070dc4485da159
breixo14 <breixodocampo@gmail.com>
2025-12-17 12:24:39 +0100

    second commit

---
+++ src/main/java/segundo/dam/tuppermania/TupperManiaApplication.java
@@ -0,0 +1,13 @@
+package segundo.dam.tuppermania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TupperManiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TupperManiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/segundo/dam/tuppermania/TupperManiaApplicationTests.java
@@ -0,0 +1,13 @@
+package segundo.dam.tuppermania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TupperManiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
