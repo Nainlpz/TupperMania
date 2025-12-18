fe7f41ae64013586c3f315b5b63e9ac286f7a763
breixo14 <breixodocampo@gmail.com>
2025-12-18 10:11:14 +0100

    feat(model): create PerfilFisico entity

---
+++ src/main/java/segundo/dam/tuppermania/model/PerfilFisico.java
@@ -0,0 +1,24 @@
+package segundo.dam.tuppermania.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "perfil_fisico")
+public class PerfilFisico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_perfil;
+
+    private Double peso;
+    private Double altura;
+    private Integer edad;
+    private String alergias;
+    private String intolerancias;
+
+    @Enumerated(EnumType.STRING)
+    private Sexo sexo; // Crear Enum Sexo
+
+    @OneToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+}
